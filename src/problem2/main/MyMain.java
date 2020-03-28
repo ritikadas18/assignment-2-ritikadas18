8b181a664005eb79dcc2fa16c3052be71f97e574
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-03-28 12:59:25 +0530

    insert method calls

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -15,6 +15,18 @@ public class MyMain {
         bstTraversal tree = new bstTraversal();
         MyBinarySearchTree mst = new MyBinarySearchTree();
         TreeNode root = new TreeNode(10);
+        mst.insert(root, 5);
+        mst.insert(root, 1);
+        mst.insert(root, 15);
+        mst.insert(root, 9);
+        mst.insert(root, 7);
+        mst.insert(root, 12);
+        mst.insert(root, 30);
+        mst.insert(root, 25);
+        mst.insert(root, 40);
+        mst.insert(root, 45);
+        mst.insert(root, 6);
+        mst.insert(root, 3);
 
     }
 }
