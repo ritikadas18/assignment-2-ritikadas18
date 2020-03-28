bacea6d90e05068c44ce071d89cd7baa9f20e9b2
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-03-28 13:36:36 +0530

    getter and setter of front variable added

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -12,5 +12,13 @@ public class MyPriorityQueue {
 
     private Node front;
 
+    public Node getFront() {
+        return front;
+    }
+
+    public void setFront(Node front) {
+        this.front = front;
+    }
+
 
 }
