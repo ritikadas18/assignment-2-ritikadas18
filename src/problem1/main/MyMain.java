ebb02434237cc409fa17342b76b091bbb48a80c5
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-03-28 12:26:14 +0530

    insert method calls added

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -12,6 +12,17 @@ public class MyMain {
     public static void main(String[] args) {
         MyBinarySearchTree bst = new MyBinarySearchTree();
         TreeNode root;
-
+        root = new TreeNode(10);
+        bst.insert(root, 7);
+        bst.insert(root, 3);
+        bst.insert(root, 12);
+        bst.insert(root, 10);
+        bst.insert(root, 6);
+        bst.insert(root, 17);
+        bst.insert(root, 26);
+        bst.insert(root, 19);
+        bst.insert(root, 36);
+        bst.insert(root, 52);
+        bst.insert(root, 1);
     }
 }
