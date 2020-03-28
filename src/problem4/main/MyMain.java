85cf30f300dc066d426a4495b3702b2008f1a004
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-03-28 15:28:18 +0530

    one more package added and insert call made

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -1,12 +1,23 @@
 package problem4.main;
 
 import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
 import problem4.myqueue.MyQueue;
 
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
     public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        TreeNode root;
+        root = new TreeNode(10);
+        obj.insert(root,5);
+        obj.insert(root,10);
+        obj.insert(root,7);
+        obj.insert(root,26);
+        obj.insert(root,13);
+        obj.insert(root,19);
+        obj.insert(root,7);
 
     }
 }
