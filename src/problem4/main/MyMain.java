9d10c3f10f5c243f0f5241e1eed152636679d832
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-03-28 14:46:06 +0530

    some changes made

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -1,15 +1,12 @@
 package problem4.main;
 
 import problem1.mybst.MyBinarySearchTree;
-import problem1.node.TreeNode;
 import problem4.myqueue.MyQueue;
-import problem4.node.Node;
-
-import java.util.ArrayList;
-import java.util.Scanner;
 
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
 
+    }
 }
