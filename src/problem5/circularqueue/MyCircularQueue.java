f6aa64d096e1e0be0667bd669f207b32ded500bb
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-03-28 15:58:25 +0530

    variables declared

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,6 +5,12 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+
+import problem5.node.Node;
 //to implement circular queue
 public class MyCircularQueue {
+    private Node front;
+    private Node rear;
+
+
 }
