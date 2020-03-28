3d570de15be7c3577168c91e2a04598e2410f78b
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-03-28 16:05:39 +0530

    deQueue method added

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -45,6 +45,22 @@ public class MyCircularQueue {
                 break;
         }
     }
+    public Node deQueue() {
+        Node temp;
+        if (getFront() == null) {
+            return null;
+        } else if (getFront() == getRear()) {
+            temp = getFront();
+            setRear(null);
+            setFront(null);
+        } else {
+            temp = getFront();
+            setFront(getFront().getNext());
+            getRear().setNext(getFront());
+        }
+        return temp;
+    }
+
 
 
 
