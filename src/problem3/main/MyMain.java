3685666e804a5950d5b15463f87366093cb01093
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-03-28 14:14:12 +0530

    final commit:problem 3 completed

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -10,7 +10,31 @@ import java.util.Scanner;
 // use problem5.student.Student class to create object of student
 public class MyMain {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         MyPriorityQueue priorityQueue = new MyPriorityQueue();
+        System.out.println("Enter number of nodes ");
+        int nodes = sc.nextInt();
+        while (nodes-- > 0) {
+            Student student = new Student();
+            System.out.println("Enter name ");
+            String name = sc.next().trim();
+            System.out.println("Enter roll number");
+            int roll = sc.nextInt();
+            student.setName(name);
+            student.setRollNumber(roll);
 
+            Node node = new Node();
+            node.setStudent(student);
+
+            priorityQueue.enQueue(node);
+        }
+
+        System.out.println("Displaying Queue");
+
+        Node node = priorityQueue.deQueue();
+        while (node != null) {
+            System.out.println(node);
+            node = priorityQueue.deQueue();
+        }
+    }
 }
