3d8767d45f47b6ae4bebd48a63553e49d45cff65
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-03-28 13:30:09 +0530

    node class completed

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -27,4 +27,10 @@ public class Node {
     public void setNextNode(Node nextNode) {
         this.nextNode = nextNode;
     }
+
+    @Override
+    public String toString() {
+        return "Node " + "\n" +
+                getStudent();
+    }
 }
