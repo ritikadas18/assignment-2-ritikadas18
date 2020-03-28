9237be2e2bee4b98b1adec2f71da72dc48e97a7c
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-03-28 16:15:55 +0530

    enter details

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -17,8 +17,19 @@ public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         MyCircularQueue cq = new MyCircularQueue();
+        System.out.println("Enter number of records you wants to enter");
+        int nOR = scan.nextInt();
+        while (nOR != 0) {
+            Student student = new Student();
+            Node node = new Node();
 
-
+            System.out.println("Enter name");
+            student.setName(scan.next().trim());
+            System.out.println("Enter roll number");
+            student.setRollNumber(scan.nextInt());
+            System.out.println("Enter backlogs");
 
     }
+
+}
 }
