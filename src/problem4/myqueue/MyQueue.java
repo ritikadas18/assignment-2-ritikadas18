d13c16abc16fe86d098c246c0772a00436ba5ace
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-03-28 14:30:09 +0530

    variables declared

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -1,11 +1,12 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:11 PM
- */
 package problem4.myqueue;
 // to create queue to store pre - order successor
+import problem1.node.TreeNode;
+import problem3.node.Node;
 
 public class MyQueue {
+
+    private Node front;
+    private Node rear;
+
+
 }
