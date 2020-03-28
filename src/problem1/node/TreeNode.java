4ec8254893c1f74e4ef14f9cd673cf577cda0b0f
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-03-28 10:46:48 +0530

    constructor created

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -12,4 +12,10 @@ public class TreeNode {
     private int data;
     private TreeNode left;
     private TreeNode right;
+
+    public TreeNode(int data) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
 }
