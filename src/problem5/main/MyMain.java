933f174d33084e6ab5c9f5742e91aa80c4064422
ritikadas18 <ritika.das_cs18@gla.ac.in>
2020-03-28 16:12:13 +0530

    basic structure of MyMain class

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,6 +5,16 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+
+import problem5.circularqueue.MyCircularQueue;
+import problem5.node.Node;
+import problem5.student.Student;
+
+import java.util.Scanner;
+
 //executable class
 public class MyMain {
+    public static void main(String[] args) {
+
+    }
 }
